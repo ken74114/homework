05a57817bc8cceb2b992d34fd33758cee6c1b591
Ken <ken74114@gmail.com>
2015-07-07 22:28:10 +0800

    該專案要在資料庫中建立一個檢視表，並且匯入 EDMX 檔。

--- Customer/Models/CustomerDB.Context.cs
+++ Customer/Models/CustomerDB.Context.cs
@@ -29,5 +29,6 @@ namespace Customer.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+        public virtual DbSet<v_rpt1> v_rpt1 { get; set; }
     }
 }
