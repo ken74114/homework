8eb92109177eab1d8c18f420811f6bb6d2d68c1b
Ken <ken74114@gmail.com>
2015-07-08 21:36:20 +0800

    實作一份簡易報表，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，用一個表格呈現報表即可。

---
+++ Customer/Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Customer.Models;
+
+namespace Customer.Controllers
+{
+    public class ReportController : Controller
+    {
+        private CustomInfoEntities db = new CustomInfoEntities();
+
+        // GET: /Report/
+        public ActionResult Index()
+        {
+            return View(db.v_rpt1.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
