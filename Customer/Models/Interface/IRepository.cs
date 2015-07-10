f85a417a45a3caef05b7e129bf1b8c4cf1711940
Ken <ken74114@gmail.com>
2015-07-10 22:47:48 +0800

    Revert "Revert "實作「客戶聯絡人」時，同一個客戶下的聯絡人，其 Email 不能重複""
    This reverts commit 21893a7b717706dd12e47ad0873f5c3a69580cb9.

---
+++ Customer/Models/Interface/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Models.Interface
+{
+    public interface IRepository<TEntity> : IDisposable
+        where TEntity : class
+    {
+        void Create(TEntity instance);
+
+        void Update(TEntity instance);
+
+        void Delete(TEntity instance);
+
+        TEntity Get(int? id);
+
+        IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+
+        void SaveChanges();
+
+    }
+}
