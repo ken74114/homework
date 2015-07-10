3efc1b520e09e25476fdeddd6b19c79e784988df
Ken <ken74114@gmail.com>
2015-07-10 20:24:17 +0800

    實作「客戶聯絡人」時，同一個客戶下的聯絡人，其 Email 不能重複

---
+++ Customer/Models/Repository/GenericRepository.cs
@@ -0,0 +1,152 @@
+﻿using Customer.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace Customer.Models.Repository
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private DbContext _context
+        {
+            get;
+            set;
+        }
+
+        public GenericRepository()
+            : this(new CustomInfoEntities())
+        {
+        }
+
+        public GenericRepository(DbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            this._context = context;
+        }
+
+        public GenericRepository(ObjectContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            this._context = new DbContext(context, true);
+        }
+
+
+
+        /// <summary>
+        /// Creates the specified instance.
+        /// </summary>
+        /// <param name="instance">The instance.</param>
+        /// <exception cref="System.ArgumentNullException">instance</exception>
+        public void Create(TEntity instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                this._context.Set<TEntity>().Add(instance);
+                this.SaveChanges();
+            }
+
+        }
+
+        /// <summary>
+        /// Updates the specified instance.
+        /// </summary>
+        /// <param name="instance">The instance.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public void Update(TEntity instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                this._context.Entry(instance).State = EntityState.Modified;
+                this.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Deletes the specified instance.
+        /// </summary>
+        /// <param name="instance">The instance.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public void Delete(TEntity instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                this._context.Entry(instance).State = EntityState.Modified;
+                this.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Gets the specified predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns></returns>
+        public TEntity Get(int? id)
+        {
+            return this._context.Set<TEntity>().Find(id);
+        }
+
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            if (predicate == null)
+            {
+                return this._context.Set<TEntity>().AsQueryable();
+            }
+
+            return this._context.Set<TEntity>().Where(predicate).AsQueryable();
+        }
+
+
+        public void SaveChanges()
+        {
+            this._context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this._context != null)
+                {
+                    this._context.Dispose();
+                    this._context = null;
+                }
+            }
+        }
+
+
+
+    }
+}
