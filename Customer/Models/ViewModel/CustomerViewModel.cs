6ac4cd796752e50c7657c345863c7b175c6cbf55
Ken <ken74114@gmail.com>
2015-07-20 22:36:41 +0800

    調整

---
+++ Customer/Models/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using PagedList;
+namespace Customer.Models.ViewModel
+{
+    public class CustomerViewModel
+    {
+        public IPagedList<客戶資料> Customers { get; set; }
+        public int PageIndex { get; set; }
+
+        public CustomerViewModel()
+        {
+            this.PageIndex = 1;
+        }
+    }
+}
