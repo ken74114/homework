2ea1a2a02de8f3a68f9ddfa3c8d91b211bf444b1
Ken <ken74114@gmail.com>
2015-07-21 20:57:20 +0800

    新增報表匯出功能

--- Customer/Controllers/ReportController.cs
+++ Customer/Controllers/ReportController.cs
@@ -1,4 +1,7 @@
 ﻿using Customer.Models;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.IO;
 using System.Web.Mvc;
 
 namespace Customer.Controllers
@@ -12,11 +15,46 @@ namespace Customer.Controllers
             return View(this.rptRepository.All());
         }
 
+        [HttpPost]
+        public ActionResult ExportExcel()
+        {
+            var data = this.rptRepository.All();
+            string path = Server.MapPath("~/Templates/客戶資訊.xlsx");
+            FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            IWorkbook wb = new XSSFWorkbook(stream);
+            stream.Close();
+            ISheet sheet = wb.GetSheetAt(0);
+            ICellStyle cs = sheet.GetRow(1).Cells[0].CellStyle;
+            int Index = 1;
+            foreach (var item in data)
+            {
+                IRow row = sheet.CreateRow(Index);
+                CreateCell(item.客戶名稱, row, 0, cs);
+                CreateCell(item.聯絡人數量.ToString(), row, 1, cs);
+                CreateCell(item.客戶帳戶數量.ToString(), row, 2, cs);
+                Index++;
+            }
+            string serverPath = @"D:/repot.xlsx";
+            using (FileStream file = new FileStream(serverPath, FileMode.Create))
+            {
+                wb.Write(file);
+                file.Close();
+            }
+            return File(serverPath, "application/excel","Report.xlsx");
+        }
+
+        private static void CreateCell(string value, IRow row, int index, ICellStyle cellStyle)
+        {
+            ICell cell = row.CreateCell(index);
+            cell.SetCellValue(value);
+            cell.CellStyle = cellStyle;
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-               this. rptRepository.UnitOfWork.Context.Dispose();
+                this.rptRepository.UnitOfWork.Context.Dispose();
             }
             base.Dispose(disposing);
         }
